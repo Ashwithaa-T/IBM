0fbe179c53b9f861d1a8bb39d0ad64ac02a065f3
Ashwithaa-T <tera.ashwithaareddy@gmail.com>
2025-06-26 13:10:43 +0530

    Create Calculator.py

---
+++ Practice/Calculator.py
@@ -0,0 +1,15 @@
+def add(var1,var2):
+    print(var1+var2)
+def sub(var1,var2):
+    print(var1 - var2)
+def mult(var1,var2):
+    print(var1* var2)
+def divide(var1,var2):
+    print(var1/var2)
+print("Enter two numbers ")
+var1 = float(input("Enter the number1 "))
+var2 = float(input("Enter the number2 "))
+add(var1,var2)
+sub(var1,var2)
+mult(var1,var2)
+divide(var1,var2)
